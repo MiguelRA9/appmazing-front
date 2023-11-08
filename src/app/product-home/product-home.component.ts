87de5ccdec96bd898165d35db1701782e4a7ff9b
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-08 14:27:26 +0100

    Bring data to table products from backend

--- src/app/product-home/product-home.component.ts
+++ src/app/product-home/product-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-home',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-home.component.css']
 })
 export class ProductHomeComponent implements OnInit {
+  products: any = []; //Este es el datasource que se coge en el html.
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'name', 'stock', 'price', 'active', 'date_added', 'category'];
 
-  ngOnInit() {
+  constructor(private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe(data => {
+      this.products = data;
+    })
   }
 
 }
