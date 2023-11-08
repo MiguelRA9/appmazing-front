9f4f9826b251099cb6993032f003407c5322666f
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-08 09:37:07 +0100

    Add openDetailForm() method

--- src/app/contact-home/contact-home.component.ts
+++ src/app/contact-home/contact-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactsService } from '../contacts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-home',
@@ -9,13 +10,17 @@ import { ContactsService } from '../contacts.service';
 export class ContactHomeComponent implements OnInit{
   contacts: any = []; //Este es el datasource que se conge en el html
 
-  constructor(private contactsService: ContactsService){};
+  constructor(private contactsService: ContactsService, private router: Router){};
 
   ngOnInit(): void {
       this.contactsService.getContacts().subscribe(data => {
         this.contacts = data;
       })
   }
+
+openDetailForm(row: any){
+  this.router.navigate(['/contact', row.id]);
+}
 
   displayedColumns: string[] = ['id', 'name', 'first_surname', 'second_surname', 'phone', 'email'];
 }
