5d54101d9d762254d223c03fd426c98883003e59
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-08 13:27:03 +0100

    Add product-home declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ContactHomeComponent } from './contact-home/contact-home.component';
 import { MatButtonModule, MatCardModule, MatIconModule, MatTableModule, MatToolbarModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { ProductHomeComponent } from './product-home/product-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactHomeComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    ProductHomeComponent
   ],
   imports: [
     BrowserModule,
