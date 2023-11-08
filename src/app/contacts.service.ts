74c491262c1004649e1bd575cccfe9aae27fc692
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-08 12:18:49 +0100

    Minor change, comment deleted

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -18,7 +18,6 @@ export class ContactsService {
   getContact(contact_id: number): Observable<any>{
     const url = 'http://localhost:30030/contacts/get';
     const headers = new HttpHeaders().set('Content-type', 'application/json');
-    //Se podría poner aquí headers.set('Content-type', 'application/json') y dejar new HttpHeaders(), si se entiende mejor.
     const body = JSON.stringify({id: contact_id});
     return this.http.post(url, body, {headers});
   } 
