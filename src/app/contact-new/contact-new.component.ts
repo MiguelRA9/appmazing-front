eee00c1690952beb307f3a166ed095ada1158d0f
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-09 13:09:07 +0100

    Adda variables and newContact() method

--- src/app/contact-new/contact-new.component.ts
+++ src/app/contact-new/contact-new.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-new.component.css']
 })
 export class ContactNewComponent implements OnInit {
+  name: string;
+  first_surname: string;
+  second_surname: string;
+  phone: string;
+  email: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  newContact(){
+    const contact = {
+      name: this.name,
+      first_surname: this.first_surname,
+      second_surname: this.second_surname,
+      phone: this.phone,
+      email: this.email
+    }
   }
 
 }
