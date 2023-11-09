46317c9dd38e36c65bd68e90d03801dcf36cc788
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-09 08:48:46 +0100

    Add getProduct() method

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -14,4 +14,11 @@ export class ProductsService {
     const headers = new HttpHeaders();
     return this.http.get<any>(url, {headers});
   }
+
+  getProduct(product_id: number): Observable<any>{
+    const url = 'http://localhost:30030/products/get';
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const body = JSON.stringify({id: product_id});
+    return this.http.post(url, body, {headers})
+  }
 }
