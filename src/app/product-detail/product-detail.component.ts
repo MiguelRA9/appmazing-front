8f175f658808a9bf2440d434b6630c37fc114ff1
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-09 09:07:01 +0100

    Implement code to fetch a single product info from backend based on id

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  product: any;
 
-  constructor() { }
+  constructor(private productsService: ProductsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.product = this.productsService.getProduct(this.route.snapshot.params['id']).subscribe(data => {
+      this.product = data;
+    })
   }
 
 }
