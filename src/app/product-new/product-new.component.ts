eed8d00410ce29a60ae6b98b5fe0f0a69ebbd3af
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-10 10:58:59 +0100

    Add variables and methods newContact, cancelInsert and navigateToHome

--- src/app/product-new/product-new.component.ts
+++ src/app/product-new/product-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-new',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-new.component.css']
 })
 export class ProductNewComponent implements OnInit {
+  name: string;
+  stock: number;
+  price: number;
+  active: boolean;
+  date_added: string;
+  //category
 
-  constructor() { }
+  constructor(private router: Router, private productsService: ProductsService) { }
 
   ngOnInit() {
+  }
+
+  newProduct() {
+    const product = {
+      name: this.name,
+      stock: this.stock,
+      price: this.price,
+      active: this.active,
+      date_added: this.date_added,
+      //category
+    }
+    this.productsService.newProduct(product);
+    this.navigateToHome();
+  }
+
+  cancelInsert() {
+    this.navigateToHome();
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/products']);
   }
 
 }
