1e03ad0b3c2a13fe26d196c5186fd0e6c0a2876b
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-14 12:45:50 +0100

    Declaration for ProductEditComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactNewComponent } from './contact-new/contact-new.component';
 import { FormsModule } from '@angular/forms';
 import { ProductNewComponent } from './product-new/product-new.component';
 import { ContactEditComponent } from './contact-edit/contact-edit.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ContactEditComponent } from './contact-edit/contact-edit.component';
     ProductDetailComponent,
     ContactNewComponent,
     ProductNewComponent,
-    ContactEditComponent
+    ContactEditComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
