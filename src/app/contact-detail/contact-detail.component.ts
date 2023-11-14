4e1d6cac13192ac2a286b96a926bc3241bb64344
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-14 12:41:25 +0100

    Implement editContact and closeContact method

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactsService } from '../contacts.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
@@ -11,11 +11,19 @@ export class ContactDetailComponent implements OnInit {
 
   contact: any;
 
-  constructor(private contactsService: ContactsService, private route: ActivatedRoute) { }
+  constructor(private contactsService: ContactsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.contactsService.getContact(this.route.snapshot.params['id']).subscribe(data =>{
       this.contact = data;
     })
+  }
+
+  editContact() {
+    this.router.navigate(['/contact/edit', this.route.snapshot.params['id']])
+  }
+
+  closeContact(){
+    this.router.navigate(['/contacts']);
   }
 }
