f332a5d76b13a5a79b8f1925d3fea69d0dec1646
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-14 13:41:07 +0100

    Implement updateProduct, cancelUpdate and navigateProductDetail methods

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+  product: any;
 
-  constructor() { }
+  constructor(private productService: ProductsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.productService.getProduct(this.route.snapshot.params['id']).subscribe(data =>{
+      this.product= data
+    })
+  }
+
+  updateProduct() {
+    this.productService.updateProduct(this.product);
+    this.navigateProductDetail();
+  }
+
+  cancelUpdate() {
+    this.navigateProductDetail();
+  }
+
+  navigateProductDetail() {
+    this.router.navigate(['/product', this.route.snapshot.params['id']])
   }
 
 }
