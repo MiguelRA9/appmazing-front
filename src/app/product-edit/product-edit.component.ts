de0c9797f492cdd2272c01deee6c858047d3ce88
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-16 14:33:23 +0100

    Date format needs checking

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CategoriesService } from '../categories.service';
 import { Product } from '../model/Product';
 import { Category } from '../model/Category';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-edit',
@@ -14,11 +15,14 @@ export class ProductEditComponent implements OnInit {
   product: Product = new Product();
   categories: []
 
-  constructor(private productService: ProductsService, private route: ActivatedRoute, private router: Router, private categoriesService: CategoriesService) { }
+  constructor(private productService: ProductsService, private route: ActivatedRoute, private router: Router, private categoriesService: CategoriesService, private datePipe: DatePipe) { }
 
   ngOnInit() {
     this.productService.getProduct(this.route.snapshot.params['id']).subscribe(data =>{
       this.product= data;
+
+      let dateFormated = this.datePipe.transform(this.product.date_added, 'dd/MM/yyyy');
+      //this.product.date_added = dateFormated;
     })
     this.categoriesService.getCategories().subscribe(data => {
       this.categories = data;
