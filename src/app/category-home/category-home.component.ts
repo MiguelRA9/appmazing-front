5ff84a0a19de38b77b22841c57366160b5da1712
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-23 10:48:38 +0100

    Obtain data for the table + editCategoryDetail method

--- src/app/category-home/category-home.component.ts
+++ src/app/category-home/category-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../categories.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-category-home',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-home.component.css']
 })
 export class CategoryHomeComponent implements OnInit {
+  categories: any = [];
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'name']
 
-  ngOnInit() {
+  constructor(private categoriesService: CategoriesService, private router: Router, public dialog: MatDialog) { };
+
+  ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe(data => {
+      this.categories = data;
+    })
   }
 
+  editCategoryDetail(category: any) {
+    this.router.navigate(['/category/edit', category])
+  }
 }
