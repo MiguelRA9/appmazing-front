27c537ee83f3db3e62ea74ba76b092ade6b1a3ef
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-11-23 12:52:16 +0100

    Add parameters, constructor + methods for new Category

--- src/app/category-new/category-new.component.ts
+++ src/app/category-new/category-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoriesService } from '../categories.service';
 
 @Component({
   selector: 'app-category-new',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-new.component.css']
 })
 export class CategoryNewComponent implements OnInit {
+  name: string;
 
-  constructor() { }
+  constructor(private router: Router, private categoriesService: CategoriesService) { }
 
   ngOnInit() {
+  }
+
+  newCategory() {
+    const category = {
+      name: this.name,
+    }
+    this.categoriesService.newCategory(category);
+    this.navigateToHome();
+  }
+
+  cancelInsert() {
+    this.navigateToHome();
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/categories']);
   }
 
 }
